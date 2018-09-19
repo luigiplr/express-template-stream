001bfc646d6414415aa5238a02c5118661eaa349
Luigi Poole <luigipoole@outlook.com>
2018-09-19 11:39:50 -0700

    add babel config

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime',
+    'lodash'
+  ]
+}
